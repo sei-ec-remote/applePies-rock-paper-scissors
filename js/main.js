6acc15faad071a4b44f20341f6755e8abb7e80b1
Timm Schoenborn <timmschoenborn@gmail.com>
2023-11-28 15:35:28 -0500

    adds render functions and initial setup for gameplay

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*------ plan of action ------*/
-//  Identify and initialize our state variables
+// Identify and initialize our state variables
 // (these are our constants and our lets pertaining to the state of our game)
 // (usually, they are the initial starting point of our elements)
 // (our starting values)
@@ -38,5 +38,63 @@ let results
 let winner
 
 /*------ functions ------*/
+// initializer function -> set up our initial state and call render()
+function init() {
+    scores = {
+        p: 0,
+        c: 0,
+        t: 0
+    }
+
+    results = {
+        p: 'r',
+        c: 'r'
+    }
+
+    winner = 't'
+
+    // eventually call render(), when render has been built
+    render()
+}
+
+init()
+// renderScores -> show how many wins/losses/ties
+function renderScores() {
+    // loop over the scores object, display the scores accordingly
+    for (let key in scores) {
+        // we're using the key to select an html element
+        const scoreEl = document.getElementById(`${key}-score`)
+        // we're using bracket notation to dynamically use object values based on a changing key
+        scoreEl.innerText = scores[key]
+    }
+}
+
+// renderResults -> show the results of player and computer choices
+function renderResults() {
+    // this looks at the results object, pulls the values for the keys P and C
+    // then applies the img from the related part of the RPS_LOOKUP object
+    // as the src for the img tag associated with the player and computer
+    pResultEl.src = RPS_LOOKUP[results.p].img
+    cResultEl.src = RPS_LOOKUP[results.c].img
+
+    // this will visually identify who won the round
+    // if this el belongs to the winner, change to purple
+    // otherwise use white
+    pResultEl.style.borderColor = winner === 'p' ? 'purple' : 'white'
+    cResultEl.style.borderColor = winner === 'c' ? 'purple' : 'white'
+}
+
+
+// render -> transfer/visualize all changes to the dom
+function render() {
+    renderScores()
+    renderResults()
+}
+// we'll do this by calling a couple other render functions through a countdown
+// getRandom function for our computer player to select a move
+
+// handleChoice -> for the player to select a move(this will be an event listener)
+
+// need a getWinner function -> determine who wins, player, computer or a tie
 
 /*------ event listeners ------*/
